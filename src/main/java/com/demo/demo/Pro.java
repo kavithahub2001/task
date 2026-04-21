98cc975e64bed31cc29c82b742ac00b7de7b886a
kavithahub2001 <kavithaprema220@gmail.com>
2026-04-21 06:50:18 +0530

    added 4

--- src/main/java/com/demo/demo/Pro.java
+++ src/main/java/com/demo/demo/Pro.java
@@ -7,6 +7,6 @@ public class Pro {
     public static void One(){
         System.out.println("1");
         System.out.println("2");
-        System.out.println("3");
+        System.out.println("4");
     }
 }
