d4cdfb771ded5843aa1cf5a6b373ee4b58c7c0ab
kavithahub2001 <kavithaprema220@gmail.com>
2026-05-11 05:42:17 +0530

    change logic in pro

---
+++ src/main/java/com/demo/demo/Pro3.java
@@ -0,0 +1,9 @@
+package com.demo.demo;
+
+public class Pro3 {
+    public static void One(){
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+    }
+}
